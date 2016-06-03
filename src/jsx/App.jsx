4abbf60ca64b2d5f6934ec60b2467feaf89895fb
Moritz Stefaner <moritz@stefaner.eu>
2016-06-03 13:55:01 +0200

    Revert "clean up, added mobx"
    This reverts commit 2f410ef7a226c7d48e30bb7beccdc0c66102ca88.

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import El from "El";
-
-// let {Stage, Sprite, DisplayObjectContainer, Text} = ReactPIXI;
-// import imagePath from "../assets/particle.png";
 
 export default class App extends React.Component {
   render() {
-  	let data = this.props.data;
-  	let els = _.range(0,100).map((d)=>{
-  		return <El data={d} />;
-  	});
-
     return <div>
-       {els}
+    		<p>App rendered</p>
+    		<p>{this.props.data.length} items loaded</p>
     	</div>
   }
 }
