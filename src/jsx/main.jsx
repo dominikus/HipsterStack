3c462463318f5825ca2146db4cfe0c4053267c50
Dominikus Baur <dominikus.baur@gmail.com>
2016-11-24 16:11:45 -0700

    minimal webpack/ES6/react config

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,19 +1,2 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import DevTools from 'mobx-react-devtools';
-
-import {loadAllDataSets, getDataSet} from 'data/dataStore';
-
-import {observer} from 'mobx-react';
-
 import "html/index.html";
 import "sass/main.sass";
-
-import uiState from "state/uiState"
-import App from 'App';
-
-ReactDOM.render(<App uiState={uiState}/>, document.getElementById('app'));
-loadAllDataSets().then((dataSets)=>{
-	console.table(getDataSet("first-dataset"));
-	uiState.dataLoaded = true;
-})
