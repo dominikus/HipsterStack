a4af1b234060afcf90aecbc4dc7c22d4a9366ea5
Dominikus Baur <dominikus.baur@gmail.com>
2017-03-30 14:46:01 -0600

    added ViewModelCollection; updated packages

---
+++ src/jsx/util/ViewModelCollection.js
@@ -0,0 +1,45 @@
+import { observe, observable, extendObservable, action } from 'mobx';
+import { assign } from 'lodash';
+
+export default class ViewModelCollection {
+  @observable viewModels = [];
+
+  constructor(models, template) {
+    this.viewModelMap = new Map();
+    this.template = template;
+
+    observe(models, (e) => {
+      // housekeeping
+      e.added.forEach((item) => {
+        // console.log("++", item)
+        this.viewModels.push(this.viewModelForObject(item));
+      });
+
+      e.removed.forEach((item) => {
+        // console.log("--", item)
+        this.viewModels.splice(this.viewModels.indexOf(this.viewModelMap.get(item)), 1);
+      });
+    }, true);
+  }
+
+  viewModelForObject(o) {
+    let m = this.viewModelMap.get(o);
+    if (!m) {
+      // no model yet, create new
+      m = this.makeViewModel(o);
+      this.viewModelMap.set(o, m);
+    }
+    return m;
+  }
+
+  makeViewModel(o) {
+    return extendObservable({
+      // update function to assign new values
+      update: action((oo) => {
+        assign(this, oo);
+      }),
+      __data: o,
+      id: o.id,
+    }, this.template);
+  }
+}
